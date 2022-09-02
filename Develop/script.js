2ac7ea6488a78835469aea9953c2a7d418bc863e
Bryant <bryantmendez8@gmail.com>
2022-09-01 22:22:20 -0500

    Added variables and function to collect data from user

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,7 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialCharacters = ["!", "@", "#", "$", "%", "?", ".", ",", ";", ":", "^", "&", "*", "/"];
-var num = [1,2,3,4,5,6,7,8,9];
-
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -18,4 +17,43 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
+
+function collectInfo() {
+  var promptLength = window.prompt("Choose a number between 8 and 128 for your password length");
+  if (promptLength <8 || promptLength >128 || isNaN(promptLength)) {
+    window.prompt("Please enter another value")
+    collectInfo();
+  }
+  var choices = [];
+  var promptUpperCase = confirm("Would you like your password to have upper case?");
+  var promptLowerCase = confirm("Would you like your password to have lowercase letters?");
+  var promptSpecialCharacters = confirm("Would you like your password to have special characters?");
+  var promptNumber = confirm("Would you like your password to have numbers?")
+
+  if (!promptUpperCase && !promptLowerCase && !promptSpecialCharacters && !promptNumber) {
+    window.alert("Invalid Input");
+    collectInfo();
+  } else {
+    writePassword();
+  }
+
+  if (promptUpperCase === "true"){
+    choices.push(promptUpperCase);
+  } if (promptLowerCase === "true"){
+    choices.push(promptLowerCase);
+   } if (promptSpecialCharacters === "true"){
+      choices.push(promptSpecialCharacters);
+  } if (promptNumber === "true"){
+    choices.push(promptNumber);
+  } else {
+    collectInfo();
+  }
+  for (var i = 0; i < promptLength; i++){
+    var pick = choices[Math.floor(Math.random() * choices.length)];
+    password.push(pick);
+  }
+
+}
+
+
+generateBtn.addEventListener("click", collectInfo);
